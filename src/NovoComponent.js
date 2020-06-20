fbb0047884465048eee0966598df2edd1bf30e11
Carolinne Santos Rodrigues <carolinne.rodrigues7@hotmail.com>
2020-06-19 21:19:34 -0300

    Create new component

---
+++ src/NovoComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NovoComponent() {
+  return (
+    <div className="App">
+      Criando um novo component
+    </div>
+  );
+}
+
+export default NovoComponent;
