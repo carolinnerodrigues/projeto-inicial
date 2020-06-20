891a26ccd0c36026b7ad9c72e873a368cc551ffa
Carolinne Santos Rodrigues <carolinne.rodrigues7@hotmail.com>
2020-06-19 22:36:25 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link, Route } from 'react-router-dom';
+import routesConfig from './routesConfig';
 import './App.css';
-import NovoComponent from './NovoComponent';
-import List from './List';
 
 class App extends Component{
 
-  constructor(props){
-    super(props)
-    this.state = {
-      name: "Carolinne",
-      email: "carolinne.rodrigues7@hotmail.com"
-    }
-    this.changeState = this.changeState.bind(this);
-    this.resetState = this.resetState.bind(this);
-    this.changeInput = this.changeInput.bind(this);
-  }
-
-  changeState(){
-    this.setState({name:"Carolinne Rodrigues"})
-  }
-  resetState(){
-    this.setState({name:"Carolinne"})
-  }
-  changeInput(event){
-    let target = event.target;
-    let index = target.name;
-
-    this.setState({
-      [index]:target.value
-    })
-  }
-
   render(){
     return (
-      <div className="App" style={{ padding:'50px'}}>
-        <div>
-          <form>
-            <label>
-              Nome: 
-              <input 
-                type="text" 
-                name="name" 
-                value={this.state.name}
-                onChange={this.changeInput}
-                style={{ marginRight: '15px' }}
-              />
-              Email: 
-              <input 
-                type="text" 
-                name="email" 
-                value={this.state.email}
-                onChange={this.changeInput}
-              />
-            </label>
-          </form>
+      <div>
+        <div className="App">
+          <Link to="/" style={{ marginRight:'15px'}}>Home</Link>
+          <Link to="/user">User</Link>
         </div>
-        <div style={{ padding:'15px' }}>
-          Nome: {this.state.name} <br/> 
-          Email:{this.state.email}
-        </div>
-        <button onClick={this.changeState} style={{ marginRight: '15px' }}> Mudar estado </button>
-        <button onClick={this.resetState} > Resetar estado </button>
-        <List />
+        {
+          routesConfig.map((value, key) => {
+            return (
+              <Route
+                key={key}
+                path={value.path}
+                component={value.component}
+                exact={value.exact}
+              />
+            )
+          })
+        }
       </div>
     );
   }
